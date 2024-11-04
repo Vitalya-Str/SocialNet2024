8cad3af2e2cf71b138e8ecd51f5180c207296a2c
Vitalya-Str <114092416+Vitalya-Str@users.noreply.github.com>
2024-11-04 23:28:26 +0500

    create app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import s from './Header.module.css'
+
+const Header = () => {
+
+    return (
+        <div className={s.header}>
+            <img src="https://e7.pngegg.com/pngimages/287/216/png-clipart-black-and-red-wings-logo-illustration-logo-phoenix-art-phoenix-leaf-logo.png" alt="logo" />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,14 @@
+import s from './SideBar.module.css'
+
+const SideBar = () => {
+
+    return (
+        <div className={s.sidebar}>
+            <div><a href="#">Profile</a></div>
+            <div><a href="#">Dialogs</a></div>
+            <div><a href="#">Users</a></div>
+        </div>
+    )
+}
+
+export default SideBar
