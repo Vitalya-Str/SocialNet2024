c893ba92fe4f55178a85e3943ea44a67fb74862c
Vitalya-Str <114092416+Vitalya-Str@users.noreply.github.com>
2025-01-02 21:27:00 +0500

    110 ChatPAge

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {autorizedTh} from "./redux/appReducer";
 import {Users} from "./components/Users/Users";
 import {Col, Layout, Menu, Row, theme} from 'antd';
 import Logo from "./images/user.png";
+import {ChatPage} from "./ChatPage/ChatPage";
 
 const {Header, Content, Sider} = Layout;
 
@@ -30,13 +31,13 @@ const App = ({autorizedTh, autorized}) => {
     }
     return (
         <Layout>
-            <Header >
+            <Header>
                 <Row>
                     <Col span={18} push={6}>
                         <HeaderContainer/>
                     </Col>
                     <Col span={6} pull={18}>
-                        <img src={Logo} alt="logo"/>
+                        <img style={{width: '50px'}} src={Logo} alt="logo"/>
                     </Col>
                 </Row>
 
@@ -63,6 +64,11 @@ const App = ({autorizedTh, autorized}) => {
                                 key: '3',
                                 icon: <Link to="/users">Users</Link>,
 
+                            },
+                            {
+                                key: '4',
+                                icon: <Link to="/chatPage">Chat</Link>,
+
                             }
 
                         ]}
@@ -86,6 +92,7 @@ const App = ({autorizedTh, autorized}) => {
                             <Route path="/profile/:userId?" element={<ProfileContainer/>} exact/>
                             <Route path="/dialogs" element={<DialogsContainer/>} exact/>
                             <Route path="/users" element={<Users/>}/>
+                            <Route path="/chatPage" element={<ChatPage/>}/>
                             <Route path="/login" element={<LoginPage/>}/>
                         </Routes>
                     </Content>
---
+++ src/ChatPage/ChatPage.tsx
@@ -0,0 +1,69 @@
+import {Button} from "antd";
+import {FC, useEffect, useState} from "react";
+
+const wsChannel = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+
+export type MessageType = {
+    message: string,
+    photo: string,
+    userId: number,
+    userName: string
+}
+
+export const ChatPage = () => {
+
+    return <div>
+        <Chat/>
+    </div>
+}
+
+const Chat = () => {
+
+
+    return <div>
+        <Messages/>
+        <ChatInput/>
+    </div>
+}
+
+const Messages = () => {
+    const [message, setMessages] = useState<MessageType[]>([])
+    useEffect(() => {
+
+        wsChannel.addEventListener('message', (e) => {
+            let newMessage = JSON.parse(e.data)
+
+            setMessages((prevMessages) => [...prevMessages, ...newMessage])
+        })
+    }, [])
+
+    return <div>
+        {message.map((m, index) => <Message key={index} message={m}/>)}
+    </div>
+}
+
+const Message: FC<any> = ({message}): any => {
+    return <div>
+        <div><img src={message.photo} alt="ava"/></div>
+        <div><b>{message.userName}</b></div>
+        <div>{message.message}</div>
+        <hr/>
+    </div>
+}
+
+const ChatInput = () => {
+    const [message, setMassage] = useState('')
+    const sendMessage = () => {
+        if (!message) {
+            return
+        }
+
+        wsChannel.send(message)
+        setMassage('')
+    }
+
+    return <div>
+        <div><textarea onChange={(e) => setMassage(e.currentTarget.value)} value={message}></textarea></div>
+        <div><Button onClick={sendMessage}>Send</Button></div>
+    </div>
+}
